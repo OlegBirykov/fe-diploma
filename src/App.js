aca55e9e5c6d75f5233d385e5860b2a971b807b0
unknown <p_petro75@mail.ru>
2021-06-25 16:43:21 +0300

    Добавлен провайдер контекста и роутер

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import TaskHeader from './TaskHeader';
+import AppProvider from './AppProvider';
+//import PostsList from './components/PostsList';
+//import NewPost from './components/NewPost';
+//import ViewPost from './components/ViewPost';
+//import EditPost from './components/EditPost';
+//import Fetch from './components/Fetch';
 
-function App() {
-  return (<React.Fragment>
-    <div className="task-1">
-      <TaskHeader title="Задача 1" />
-    </div>
-    <div className="task-2">
-      <TaskHeader title="Задача 2" />
-    </div>
-  </React.Fragment>);
+function App() {    
+  return (
+    <AppProvider>
+      <Router>
+          <Header />
+          <Switch>
+            <Route path={process.env.PUBLIC_URL + '/posts/new'} component={NewPost} />
+            <Route path={process.env.PUBLIC_URL} component={PostsList} />
+          </Switch> 
+          <Footer />
+      </Router>
+    </AppProvider>
+  );
 }
 
 export default App;
- 
