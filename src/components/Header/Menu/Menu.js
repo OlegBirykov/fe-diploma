a499b6f5c01409111564beba1b023dab00a76d1e
OlegBirykov <p_petro75@mail.ru>
2021-07-01 21:23:13 +0300

    Заблокированы действия меню по умолчанию

--- src/components/Header/Menu/Menu.js
+++ src/components/Header/Menu/Menu.js
@@ -2,19 +2,23 @@ import { NavLink } from 'react-router-dom';
 import './Menu.css';
 
 function Menu() {
+  const menuItemClick = (evt) => {
+    evt.preventDefault();
+    console.log('Выбран пункт меню');
+  }
   
   return (
     <nav className="menu">
-      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active" exact>
+      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active" onClick={menuItemClick}>
         О нас
       </NavLink>
-      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active">
+      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active" onClick={menuItemClick}> 
         Как это работает
       </NavLink>
-      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active">
+      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active" onClick={menuItemClick}>
         Отзывы
       </NavLink>
-      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active">
+      <NavLink to={process.env.PUBLIC_URL} className="menu__item" activeClassName="menu__item_active" onClick={menuItemClick}>
         Контакты
       </NavLink>
     </nav>    
