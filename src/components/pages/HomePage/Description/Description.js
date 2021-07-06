a6d9f4d4fd6abf7ed797871b6464604c6179d762
unknown <p_petro75@mail.ru>
2021-07-06 16:45:03 +0300

    Работа над главной страницей

--- src/components/pages/HomePage/Description/Description.js
+++ src/components/pages/HomePage/Description/Description.js
@@ -1,11 +1,34 @@
+import { Link } from 'react-router-dom';
 import './Description.css';
 
 function Description() {
+  const description = [
+    'Удобный заказ на сайте',
+    'Нет необходимости ехать в офис',
+    'Огромный выбор направлений', 
+  ];
+
   return (
     <div className="description"> 
-      <f2>
-        Как это работает
-      </f2>
+      <div className="description__top">
+        <f2 className="description__title">
+          Как это работает
+        </f2>
+        <Link to={process.env.PUBLIC_URL} className="description__link">
+          Узнать больше
+        </Link>
+      </div>
+      <div className="description__items">
+        {description.map((item, index) => 
+          <div className="description__item">
+            <div className={`description__image description__image_${index + 1}`}>
+            </div>
+            <p className="description__text">
+              {item}
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
