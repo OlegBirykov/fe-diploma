b99cecbec02a7adde4c005ac4b55c1693c19d568
OlegBirykov <p_petro75@mail.ru>
2021-07-07 22:18:34 +0300

    Добавлена разметка и стили элементов главной страницы до подвала

--- src/components/pages/HomePage/Feedbacks/Feedbacks.js
+++ src/components/pages/HomePage/Feedbacks/Feedbacks.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './Feedbacks.css';
 import data from './data'; 
 
@@ -9,9 +9,10 @@ function Feedbacks() {
     setPageNumber(number);
   };
 
-  const points = (new Array(Math.trunc((data.length + 1) / 2))).map((item, index) => 
+  const points = data.filter((item, index) => !(index % 2)).map((item, index) =>
     <div 
       className={'feedbacks__point' + (pageNumber === index ? ' feedbacks__point_active' : '')}
+      key={index}
       onClick={() => selectPage(index)} 
     >
     </div>
@@ -23,6 +24,29 @@ function Feedbacks() {
         Отзывы
       </h2>
       <div className="feedbacks__items">
+        {data.filter((item, index) => Math.trunc(index / 2) === pageNumber).map((item, index) => 
+          <div className="feedbacks__item" key={index}>
+            <img className="feedbacks__avatar" src={item.avatar} alt="avatar" />
+            <div className="feedbacks__content">
+              <p className="feedbacks__name">
+                {item.name}
+              </p>
+              <p className="feedbacks__text">
+                <span className="feedbacks__quote feedbacks__quote_begin">
+                  &#x201C;
+                </span>
+                {item.text.map((line, index) => 
+                  <React.Fragment key={index}>{line}
+                    {(index < item.text.length - 1) && <br/>}
+                  </React.Fragment>
+                )}
+                <span className="feedbacks__quote feedbacks__quote_end">
+                  &nbsp;&#x201E;
+                </span>
+              </p>
+            </div>
+          </div>
+        )}
       </div>
       <div className="feedbacks__points">
         {points}
