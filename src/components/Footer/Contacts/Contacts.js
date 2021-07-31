19cc7b6636b9446d97c79a3a27e4fdfc0b58113c
OlegBirykov <p_petro75@mail.ru>
2021-07-31 21:42:06 +0300

    Работа с иконками svg подвала

--- src/components/Footer/Contacts/Contacts.js
+++ src/components/Footer/Contacts/Contacts.js
@@ -1,11 +1,39 @@
 import './Contacts.css';
+import icons from './icons.svg';
 
 function Contacts() {  
   return (
     <div className="contacts">
-      <h3>
+      <h3 className="contacts__title">
         Свяжитесь с нами
-      </h3> 
+      </h3>
+      <ul className="contacts__list">
+        <li className="contacts__list-item">
+          <a className="contacts__link" href="tel:+78000000000">
+            <svg className="contacts__icon" width="30" height="30">
+              <use xlinkHref={icons + '#phone'} />
+            </svg>
+          </a>
+          <p className="contacts__text">
+            8 (800) 000 00 00
+          </p>
+        </li>
+        <li className="contacts__list-item">
+          <svg className="contacts__icon" width="30" height="25">
+            <use xlinkHref={icons + '#email'} />
+          </svg>
+        </li>
+        <li className="contacts__list-item">
+          <svg className="contacts__icon" width="30" height="30">
+            <use xlinkHref={icons + '#skype'} />
+          </svg>
+        </li>
+        <li className="contacts__list-item">
+          <svg className="contacts__icon" width="21" height="30">
+            <use xlinkHref={icons + '#location'} />
+          </svg>
+        </li>
+      </ul>
     </div>
   )
 }
