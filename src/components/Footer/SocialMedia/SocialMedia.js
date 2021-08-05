d49265a04ee875d52c61624bdb43a1f8edcb0124
OlegBirykov <p_petro75@mail.ru>
2021-08-05 22:28:14 +0300

    Добавлены стили подвала

--- src/components/Footer/SocialMedia/SocialMedia.js
+++ src/components/Footer/SocialMedia/SocialMedia.js
@@ -1,4 +1,5 @@
 import './SocialMedia.css';
+import icons from './icons.svg';
 
 function SocialMedia() {  
   return (
@@ -6,6 +7,43 @@ function SocialMedia() {
       <h3 className="social-media__title">
         Подписывайтесь на нас
       </h3>
+      <ul className="social-media__list">
+        <li className="social-media__list-item">
+          <a className="social-media__link" href="https://www.youtube.com">
+            <svg className="social-media__icon" width="38" height="30">
+              <use xlinkHref={icons + '#youtube'} />
+            </svg>
+          </a>
+        </li>
+        <li className="social-media__list-item">
+          <a className="social-media__link" href="https://www.instagram.com">
+            <svg className="social-media__icon" width="32" height="30">
+              <use xlinkHref={icons + '#instagram'} />
+            </svg>
+          </a>
+        </li>
+        <li className="social-media__list-item">
+          <a className="social-media__link" href="https://plus.google.com">
+            <svg className="social-media__icon" width="47" height="30">
+              <use xlinkHref={icons + '#google-plus'} />
+            </svg>
+          </a>
+        </li>
+        <li className="social-media__list-item">
+          <a className="social-media__link" href="https://www.facebook.com">
+            <svg className="social-media__icon" width="14" height="30">
+              <use xlinkHref={icons + '#facebook'} />
+            </svg>
+          </a>
+        </li>  
+        <li className="social-media__list-item">
+          <a className="social-media__link" href="https://twitter.com">
+            <svg className="social-media__icon" width="37" height="30">
+              <use xlinkHref={icons + '#twitter'} />
+            </svg>            
+          </a>
+        </li>      
+      </ul>
     </div>
   )
 }
