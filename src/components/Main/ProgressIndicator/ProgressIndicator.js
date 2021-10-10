a31ec7dde964377cca4091b68f21b3a7f8a0525b
OlegBirykov <p_petro75@mail.ru>
2021-10-10 18:16:07 +0300

    Добавлены разметка и стили компонента ProgressIndicator

--- src/components/Main/ProgressIndicator/ProgressIndicator.js
+++ src/components/Main/ProgressIndicator/ProgressIndicator.js
@@ -1,13 +1,42 @@
 import PropTypes from 'prop-types';
 import './ProgressIndicator.css';
+import darkTriangle from './dark-triangle.svg';
+import lightTriangle from './light-triangle.svg';
 
 function ProgressIndicator(props) {
   const { stepNumber } = props;
 
+  const steps = [
+    'Билеты',
+    'Пассажиры',
+    'Оплата',
+    'Проверка'
+  ];
+
   return (
     <div className="progress-indicator"> 
-      Прогресс - индикатор! {stepNumber}
-    </div>    
+      {steps.map((item, i, array) => {
+        let stepClassName = `progress-indicator__step progress-indicator__step_${i + 1}`;
+        if (i < stepNumber) {
+          stepClassName += ' progress-indicator__step_highlighted';
+        }
+        const stepNameClassName = `progress-indicator__step-name progress-indicator__step-name_${i + 1}`;
+
+        return (
+          <div className={stepClassName} key={i}>
+            <p className="progress-indicator__step-number">
+              {i + 1}
+            </p>
+            <p className={stepNameClassName}>
+              {item}
+            </p>
+            {(i < array.length - 1) && 
+              <img className="progress-indicator__triangle" src={(i < stepNumber) ? lightTriangle : darkTriangle} width="37" height="98" alt="triangle" />
+            }
+          </div>
+        )
+      })}
+    </div>   
   )
 }
 
