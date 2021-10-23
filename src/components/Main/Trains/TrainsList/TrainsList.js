a91ff62209de593e23d2b4a5a0f785c69ad0b106
OlegBirykov <p_petro75@mail.ru>
2021-10-24 00:13:09 +0300

    Добавлена сортировка и количество поездов на странице

--- src/components/Main/Trains/TrainsList/TrainsList.js
+++ src/components/Main/Trains/TrainsList/TrainsList.js
@@ -1,33 +1,99 @@
-import { useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './TrainsList.css';
 import AppContext from 'AppContext';
-import SortingSelection from './SortingSelection/SortingSelection';
-import LimitSelection from './LimitSelection/LimitSelection';
 import TrainsListItem from './TrainsListItem/TrainsListItem';
 import shortid from 'shortid';
 
 function TrainsList(props) {
   const { reloadInfo } = props; 
 
+  const [sortIndex, setSortIndex] = useState(0);
+  const [limitIndex, setLimitIndex] = useState(0);
+  const [showList, setShowList] = useState(false);
+
   const { trainsInfo } = useContext(AppContext);
+
+  useEffect(() => {
+    switch (trainsInfo.params.sort) {
+      case 'price_': 
+        setSortIndex(1);
+        break;
+      case 'duration':
+        setSortIndex(2);
+        break;
+      default:
+        setSortIndex(0);
+    }
+  }, [trainsInfo.params.sort]);
+
+  useEffect(() => {
+    switch (+trainsInfo.params.limit) {
+      case 10: 
+        setLimitIndex(1);
+        break;
+      case 20:
+        setLimitIndex(2);
+        break;
+      default:
+        setLimitIndex(0);
+    }
+  }, [trainsInfo.params.limit]);
 
   const isForward = trainsInfo.params.direction === 'forward';
   const trainsArray = isForward ? trainsInfo.forwardTrainsInfo.items : trainsInfo.backwardTrainsInfo.items;
   const trainsCount = isForward ? `туда: ${trainsInfo.forwardTrainsInfo.total_count}` : `обратно: ${trainsInfo.backwardTrainsInfo.total_count}`
 
+  const sortModes = [
+    { mode: 'date', name: 'времени' },
+    { mode: 'price_', name: 'стоимости' },
+    { mode: 'duration', name: 'длительности' }
+  ];
+
+  const limits = [5, 10, 20];
+
+  const selectSort = (index) => {
+    setShowList(false);
+    setSortIndex(index);
+    reloadInfo({ sort: sortModes[index].mode });
+  }
+
+  const selectLimit = (index) => {
+    setLimitIndex(index);
+    reloadInfo({ limit: limits[index] });
+  }
+
   return (
     <div className="trains-list"> 
       <div className="trains-list__menu">
-        <p className="trains-list__menu-item-count">
-          Найдено поездов {trainsCount}
+        <p className="trains-list__menu-count">
+          найдено поездов {trainsCount}
         </p>
-        <div className="trains-list__menu-item-sort">
-          <SortingSelection />
+        <p className="trains-list__menu-sort-title">
+          сортировать по:
+        </p>
+        <div className="trains-list__menu-sort-value-container">
+          <p className="trains-list__menu-sort-value" onClick={() => setShowList(true)}>
+            {sortModes[sortIndex].name}
+          </p>
+          {showList &&
+            <ul className="trains-list__menu-sort-list">
+              {sortModes.map((item, i) =>
+                <li className="trains-list__menu-sort-list-item" key={i} onClick={() => selectSort(i)}>
+                  {item.name}
+                </li> 
+              )}
+            </ul>
+          }
         </div>
-        <div className="trains-list__menu-item-limit">
-          <LimitSelection />
-        </div>
+        <p className="trains-list__menu-limit-title">
+          показывать по:
+        </p>
+        {limits.map((item, i) => 
+          <p className={'trains-list__menu-limit-value' + (i === limitIndex ? ' trains-list__menu-limit-value_active' : '')} key={i} onClick={() => selectLimit(i)}>
+            {item}
+          </p> 
+        )}  
       </div>
       <div className="trains-list__list">
         {trainsArray.length &&
