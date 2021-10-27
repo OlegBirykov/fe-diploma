3d125e436453d4780b7e769c8bdd2859f1d471e9
unknown <p_petro75@mail.ru>
2021-10-27 16:44:17 +0300

    Работа с компонентом TrainsParams

--- src/components/Main/Trains/TrainsParams/TrainsParams.js
+++ src/components/Main/Trains/TrainsParams/TrainsParams.js
@@ -1,13 +1,33 @@
 //import { useContext } from 'react';
 import './TrainsParams.css';
+import DateInput from 'components/Header/TicketSearchForm/DateInput/DateInput';
 
-function TicketSearchForm() {
+function TrainsParams() {
 
   return (
     <div className="trains-params"> 
-      Здесь будут параметры выбора поезда
+      <section className="trains-params__dates">
+      <label className="trains-params__date-title">
+        Дата поездки
+        <div className="trains-params__date-container">
+          <DateInput name='left-date' value={null} placeholder='ДД ММ ГГГГ' setValue={null} />
+        </div>    
+      </label>
+      </section>
+      <section className="trains-params__options">
+        Опции
+      </section>
+      <section className="trains-params__price">
+        Цена
+      </section>
+      <section className="trains-params__times">
+        Туда
+      </section>
+      <section className="trains-params__times">
+        Обратно
+      </section>
     </div>    
   )
 }
 
-export default TicketSearchForm;
+export default TrainsParams;
