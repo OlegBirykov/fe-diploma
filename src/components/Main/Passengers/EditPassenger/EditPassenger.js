7ba0306dd53833fde6aae65d0cee9b0f2ec6f6be
OlegBirykov <p_petro75@mail.ru>
2021-12-04 23:19:18 +0300

    Работа с формой ввода данных пассажира

--- src/components/Main/Passengers/EditPassenger/EditPassenger.js
+++ src/components/Main/Passengers/EditPassenger/EditPassenger.js
@@ -1,25 +1,46 @@
-import { useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './EditPassenger.css';
 import AppContext from 'AppContext';
 
 function EditPassenger(props) {
-  const { passenger, index, passengerList, setPassengerList} = props;
-  const { 
-    isCollapsed = false,
-    //isReady = false
-  } = passenger;
+  const { passenger, index, passengerList } = props;
+
+  const [passengerState, setPassengerState] = useState({}); 
+  const [ageListShow, setAgeListShow] = useState(false);
+//  const [documentTypeListShow, setDocumentTypeListShow] = useState(false);
+
+  useEffect(() => {
+    setPassengerState({ ...passenger });
+  }, [passenger]);  
 
   const { orderInfo, setOrderInfo } = useContext(AppContext);
 
-  const setPropertyValue = (name, value) => {
-    const list = [...passengerList];
-    list[index][name] = value;
-    setPassengerList(list);
+  const { 
+    isCollapsed = false,
+    isAdult = true,
+    firstName = '',
+    lastName = '',
+    patronymic = '',
+    gender = true,
+    birthday = '',
+    isDisability = false,
+    includeChild = false,
+    documentType = 'паспорт',
+    documentData1 = '',
+    documentData2 = '',
+//    isChange = false,
+//    isError = false,
+//    isReady = false 
+  } = passengerState;
+
+  const setValue = (name, value) => {
+    setPassengerState({ ...passengerState, [name]: value});
   }
 
-  const collapse = () => {
-    setPropertyValue('isCollapsed', !isCollapsed);
+  const setIsAdult = (value) => {
+    setValue('isAdult', value);
+    setAgeListShow(false);
   }
 
   const deletePassenger = () => {
@@ -33,7 +54,10 @@ function EditPassenger(props) {
   return (
     <div className="edit-passenger">
       <div className={'edit-passenger__collapsed' + (isCollapsed ? '' : ' edit-passenger__collapsed_gray')}>
-        <button className={'edit-passenger__button-collapse' + (isCollapsed ? '' : ' edit-passenger__button-collapse_minus')} type="button" onClick={collapse}>
+        <button 
+          className={'edit-passenger__button-collapse' + (isCollapsed ? '' : ' edit-passenger__button-collapse_minus')} 
+          type="button" 
+          onClick={() => setValue('isCollapsed', !isCollapsed)}>
         </button>
         <p className="edit-passenger__title">
           Пассажир {index + 1}
@@ -45,7 +69,93 @@ function EditPassenger(props) {
       </div>
       {!isCollapsed &&
         <form className="edit-passenger__form">
-          Форма с данными
+          <div className="edit-passenger__ago-container">
+            <input 
+              className="edit-passenger__input edit-passenger__ago edit-passsenger__listbox" 
+              type="text" 
+              name="age" 
+              value={isAdult ? 'Взрослый' : 'Детский'} 
+              readOnly
+              onClick={() => setAgeListShow(true)}
+              onBlur={() => setTimeout(() => setAgeListShow(false), 1000)}
+            />
+            {ageListShow &&
+              <ul className="edit-passenger__list">
+                <li className="edit-passenger__list-item" onClick={() => setIsAdult(false)}>
+                  Детский
+                </li>
+                <li className="edit-passenger__list-item" onClick={() => setIsAdult(true)}>
+                  Взрослый
+                </li>
+              </ul>
+            }
+          </div>
+          <div className="edit-passenger__name-section">
+            <label className="edit-passenger__label">
+              Фамилия
+              <input 
+                className="edit-passenger__input" 
+                type="text" 
+                name="last-name" 
+                placeholder="Иванов" 
+                value={lastName} 
+                required
+                onChange={(evt) => setValue('lastName', evt.target.value)}
+              />
+            </label>
+            <label className="edit-passenger__label">
+              Имя
+              <input className="edit-passenger__input" type="text" name="first-name" placeholder="Иван" value={firstName} required/>
+            </label>
+            <label className="edit-passenger__label">
+              Отчество
+              <input className="edit-passenger__input" type="text" name="patronymic" placeholder="Иванович" value={patronymic} required/>
+            </label>
+          </div>
+          <div className="edit-passenger__data-section">
+            <label className="edit-passenger__label">
+              Пол
+              <div className={'edit-passenger__male' + (gender ? ' edit-passenger__select-gender' : '')}>
+                М
+              </div>
+              <div className={'edit-passenger__female' + (gender ? '' : ' edit-passenger__select-gender')}>
+                Ж
+              </div>
+            </label>
+            <label className="edit-passenger__label">
+              Дата рождения
+              <input className="edit-passenger__input" type="text" name="birthday" placeholder="ДД ММ ГГГГ" value={birthday} required/>
+            </label>
+          </div>
+          <div className="edit-passenger__options-section">
+            <label className="edit-passenger__label">
+              ограниченная подвижность
+              <input className="edit-passenger__checkbox" type="checkbox" name="disability" checked={isDisability}/>
+            </label>
+            <label className="edit-passenger__label">
+              провоз ребёнка "без места"
+              <input className="edit-passenger__checkbox" type="checkbox" name="include-child" checked={includeChild}/>
+            </label>
+          </div>
+          <div className="edit-passenger__document-data-section">
+            <label className="edit-passenger__label">
+              Тип документа
+              <input className="edit-passenger__input" type="text" name="" value={documentType} readOnly/>
+            </label>
+            <label className="edit-passenger__label">
+              Серия
+              <input className="edit-passenger__input" type="text" name="" placeholder="______" value={documentData1} required/>
+            </label>
+            <label className="edit-passenger__label">
+              Номер
+              <input className="edit-passenger__input" type="text" name="" placeholder="_________" value={documentData2} required/>
+            </label>
+          </div>
+          <div className="edit-passenger__state-section">
+            <button className="edit-passenger__button-save" type="submit" onClick={null}> 
+              Сохранить изменения 
+            </button>            
+          </div>
         </form>
       }
     </div>    
