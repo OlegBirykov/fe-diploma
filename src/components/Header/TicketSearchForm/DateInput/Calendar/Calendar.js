5ed47e909983d27484fb45e655318f576f8bee3f
OlegBirykov <p_petro75@mail.ru>
2021-12-16 21:19:41 +0300

    Работа над компонентом выбора даты

--- src/components/Header/TicketSearchForm/DateInput/Calendar/Calendar.js
+++ src/components/Header/TicketSearchForm/DateInput/Calendar/Calendar.js
@@ -1,14 +1,64 @@
-//import { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import './Calendar.css';
-//import buttonIcon from './date.svg';
 
 function Calendar(props) {
-  const { date/*, selectDate */} = props;
+//  const { date/*, selectDate */} = props;
 
   return (
     <div className="calendar" tabIndex="9999">
-      {date}
+      <div className="calendar__month">
+        <div className="calendar__month-dec">
+        </div>
+        <p className="calendar__month-name">
+          Август
+        </p>
+        <div className="calendar__month-inc">
+        </div>
+      </div>
+      <div className="calendar__days-container">
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+        <p className="calendar__day">
+          00
+        </p>
+      </div>
     </div>
   )
 }
