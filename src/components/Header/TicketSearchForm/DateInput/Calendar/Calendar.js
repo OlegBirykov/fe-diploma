c31d59dd2bd032eaf547ed2fff2c91e5d5777c77
OlegBirykov <p_petro75@mail.ru>
2021-12-18 14:58:25 +0300

    Добавлен выбор месяца

--- src/components/Header/TicketSearchForm/DateInput/Calendar/Calendar.js
+++ src/components/Header/TicketSearchForm/DateInput/Calendar/Calendar.js
@@ -1,18 +1,95 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Calendar.css';
+import moment from 'moment';
 
 function Calendar(props) {
-//  const { date, selectDate } = props;
+  const { date, selectDate } = props;
+
+  const format = 'DD.MM.YYYY';
+  const curSelectedDay = moment(date, format);
+
+  const [monthArray, setMonthArray] = useState([]);
+  const [monthIndex, setMonthIndex] = useState(0);
+  const [minDay, setMinDay] = useState(moment());
+  const [maxDay, setMaxDay] = useState(moment());
+
+  useEffect(() => {
+    const now = moment();
+    const _minDay = moment(now).add(1, 'days');
+    const _maxDay = moment(now).add(90, 'days');
+    setMinDay(_minDay);
+    setMaxDay(_maxDay);
+  
+    const minMonthFirstDay = moment(_minDay).set('date', 1);
+    const maxMonthFirstDay = moment(_maxDay).set('date', 1);
+  
+    const _monthArray = [minMonthFirstDay];
+    let index = 0;
+    while (_monthArray[index].get('month') !== maxMonthFirstDay.get('month')) {
+      _monthArray.push(moment(_monthArray[index]).add(1, 'month'));
+      index++;
+    }
+    setMonthArray(_monthArray);
+  }, []);
+
+  const monthDecEnabled = monthIndex > 0;
+  const monthIncEnabled = monthIndex < monthArray.length - 1;
+
+  const monthDec = () => {
+    if (monthDecEnabled) {
+      setMonthIndex(monthIndex - 1);
+    }
+  }
+
+  const monthInc = () => {
+    if (monthIncEnabled) {
+      setMonthIndex(monthIndex + 1);
+    }
+  } 
+
+  const monthName = (number) => {
+    switch(number) {
+      case 0:
+        return 'Январь';
+      case 1:
+        return 'Февраль';
+      case 2:
+        return 'Март';
+      case 3:
+        return 'Апрель';  
+      case 4:
+        return 'Май';
+      case 5:
+        return 'Июнь';
+      case 6:
+        return 'Июль';
+      case 7:
+        return 'Август';
+      case 8:
+        return 'Сентябрь';
+      case 9:
+        return 'Октябрь';
+      case 10:
+        return 'Ноябрь';
+      case 11:
+        return 'Декабрь';  
+      default: 
+        return '';           
+    }
+  }
+
+  console.log(selectDate, curSelectedDay, minDay, maxDay); // убрали предупреждения
 
   return (
     <div className="calendar" tabIndex="9999">
       <div className="calendar__month">
-        <div className="calendar__month-page calendar__month-page_dec">
+        <div className={'calendar__month-page calendar__month-page_dec' + (monthDecEnabled ? '' : ' calendar__month-page_dec-disabled')} onClick={monthDec}>
         </div>
         <p className="calendar__month-name">
-          Август
+          {monthName(monthArray[monthIndex].get('month'))}
         </p>
-        <div className="calendar__month-page calendar__month-page_inc">
+        <div className={'calendar__month-page calendar__month-page_inc' + (monthIncEnabled ? '' : ' calendar__month-page_inc-disabled')} onClick={monthInc}>
         </div>
       </div>
       <div className="calendar__days-container">
